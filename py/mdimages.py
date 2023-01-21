fe7644688056430fee15bf6ce84de4aa82299e54
Carsten Wulff <carsten@wulff.no>
2023-01-21 09:51:52 +0100

    Added images

---
+++ py/mdimages.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+
+import sys
+import re
+
+for l in sys.stdin:
+
+    m = re.search("!\[[^\]]+\]\((.*)\)",l)
+    if(m):
+        url = m.group(1)
+        if(re.search("^http:",url)):
+            continue
+        print(m.group(1))
